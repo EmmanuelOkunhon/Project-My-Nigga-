b686102577fa7a3aa9c29ba5fba1d81281de66c6
Emmanuel Okunhon <emmanuelokunhon@gmail.com>
2015-01-28 22:33:09 +0100

    Correction du code

--- chiffrementImages.py
+++ chiffrementImages.py
@@ -1,16 +1,11 @@
-def intistaotdz(a,b):
+def xor(a,b):
     if bool(a) != bool(b):
         return True
     else:
         return False
 
 def conv(c):
-     return str(bin(c)[2:].zfill(8))
+    return str(bin(c)[2:].zfill(8))
 
-print(conv(10))
-
-
-
-
-
+print(conv(0))
 
