781028cc5cc7cf732b197e9154cdd0aa05fe03c5
Emmanuel Okunhon <emmanuelokunhon@gmail.com>
2015-01-28 22:31:08 +0100

    Début du code

---
+++ chiffrementImages.py
@@ -0,0 +1,16 @@
+def intistaotdz(a,b):
+    if bool(a) != bool(b):
+        return True
+    else:
+        return False
+
+def conv(c):
+     return str(bin(c)[2:].zfill(8))
+
+print(conv(10))
+
+
+
+
+
+
